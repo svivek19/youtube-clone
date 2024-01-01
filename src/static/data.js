49f9c75b7a71696443092423697c1a5d9aec700f
svivek19 <svivek030503@gmail.com>
2024-01-01 11:30:46 +0530

    sidebar icons imported and exported

---
+++ src/static/data.js
@@ -0,0 +1,40 @@
+import { AiFillHome } from 'react-icons/ai'
+import { MdOutlineSubscriptions } from 'react-icons/md'
+import { SiYoutubemusic, SiYoutubegaming } from 'react-icons/si'
+import { MdVideoLibrary, MdOutlineLocalMovies } from 'react-icons/md'
+import { VscHistory } from 'react-icons/vsc'
+import { AiOutlinePlaySquare } from 'react-icons/ai'
+import { AiOutlineClockCircle, AiOutlineBulb } from 'react-icons/ai'
+import { BiLike } from 'react-icons/bi'
+import { BsNewspaper } from 'react-icons/bs'
+import { HiOutlineFire } from 'react-icons/hi'
+import { IoMusicalNoteOutline } from 'react-icons/io5'
+import { CiTrophy } from 'react-icons/ci'
+import { GiDress } from 'react-icons/gi'
+import { HiSignal } from 'react-icons/hi2'
+
+export const SidebarItem = {
+    Top:[
+        {icon:<AiFillHome size={21}/>, name:"Home"},
+        {icon:<SiYoutubemusic size={21}/>, name:"Shorts"},
+        {icon:<MdOutlineSubscriptions size={21}/>, name:"Subscriptions"},
+    ],
+    Middle:[
+        {icon:<MdVideoLibrary size={21}/>, name:"Library"},
+        {icon:<VscHistory size={21}/>, name:"History"},
+        {icon:<AiOutlinePlaySquare size={21}/>, name:"Your videos"},
+        {icon:<AiOutlineClockCircle size={21}/>, name:"Watch later"},
+        {icon:<BiLike size={21}/>, name:"Liked videos"},
+    ],
+    Explore:[
+        {icon:<HiOutlineFire size={21}/>, name:"Trending"},
+        {icon:<IoMusicalNoteOutline size={21}/>, name:"Music"},
+        {icon:<MdOutlineLocalMovies size={21}/>, name:"Moives"},
+        {icon:<HiSignal size={21}/>, name:"Live"},
+        {icon:<SiYoutubegaming size={21}/>, name:"Gaming"},
+        {icon:<BsNewspaper size={21}/>, name:"News"},
+        {icon:<CiTrophy size={21}/>, name:"Sports"},
+        {icon:<AiOutlineBulb size={21}/>, name:"Learning"},
+        {icon:<GiDress size={21}/>, name:"Fashion & beauty"},
+    ],
+}
