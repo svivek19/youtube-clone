3b6b3136937fb55ea8799827008d8c4dd2ccd2db
svivek19 <svivek030503@gmail.com>
2024-01-01 18:31:49 +0530

    some changes in ui

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,8 +4,8 @@ import '../App.css'
 
 export default function Sidebar() {
     return (
-        <div className='yt-scrollbar w-60 bg-black h-[calc(100vh-53px)] mt-14 top-0 left-0 text-white p-3 overflow-scroll'>
-            sidebar
+        <div className='yt-scrollbar w-60 bg-black h-screen top-0 left-0 text-white p-3 overflow-scroll'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur nemo aliquam, blanditiis dicta, maiores accusamus esse mollitia perferendis, dolore odit officiis non Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum molestiae itaque maiores vel vitae doloremque vero obcaecati laborum tempora. Ab tempore nulla natus illum veritatis nobis ducimus ea nisi quae? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi officia aperiam, maxime porro cupiditate ducimus, quas dolore aliquam doloremque, eos laboriosam reiciendis voluptatum perspiciatis tenetur sunt repellat est ipsa eum. illum rerum unde in modi molestias beatae ab! Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe at hic repellendus nemo optio iusto culpa ab odit, impedit recusandae explicabo excepturi labore molestiae quae totam placeat tenetur, accusantium iste.
         </div>
     )
 }
