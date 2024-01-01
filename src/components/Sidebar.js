5c606c72f1378502be62a7e29fa86a1e01f936c2
svivek19 <svivek030503@gmail.com>
2024-01-01 12:29:51 +0530

    side bar setup

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { SidebarItem } from '../static/data'
+import '../App.css'
+
+export default function Sidebar() {
+    return (
+        <div className='yt-scrollbar w-60 bg-black h-[calc(100vh-53px)] mt-14 top-0 left-0 text-white p-3 overflow-scroll'>
+            sidebar
+        </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar'
 
 const Home = () => {
   return (
-    <div>
-      home
+    <div> 
+      <Sidebar />
     </div>
   )
 }
