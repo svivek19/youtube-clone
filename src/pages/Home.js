1044dee7a4895562ceecab10c9c1c3ab22da39d4
svivek19 <svivek030503@gmail.com>
2024-01-02 17:02:02 +0530

    categories section complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,13 +8,13 @@ const Home = () => {
       <div>
         <Sidebar />
       </div>
-      <div className='w-[calc(100%-240px)] h-[calc(100%-53px)] pt-14 bg-black'>
-        <div className='flex flex-row overflow-x-scroll relative scrollbar-hide'>
+      <div className='w-[calc(100%-240px)] h-[calc(100%-53px)] pt-16 bg-black pb-3'>
+        <div className='flex flex-row overflow-x-scroll relative scrollbar-hide ml-3'>
           {
             CategoryItems.map((item, i) => {
               return (
-                <h2 key={i} className='text-white text-sm px-4 py-2 break-keep whitespace-nowrap mr-3 font-normal cursor-pointer dim-gray rounded-xl'>{item}</h2>
-              )
+                <h2 key={i} className='text-white text-sm px-4 py-2 break-keep whitespace-nowrap mr-3 font-normal cursor-pointer dim-gray hover:bg-[#1d1d1d] rounded-2xl'>{item}</h2>
+              ) 
             })
           }
         </div>
