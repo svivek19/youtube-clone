d2d8950e1676a89190f2aefab45be9055b756fcb
svivek19 <svivek030503@gmail.com>
2024-01-02 10:21:38 +0530

    side bar finish

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SidebarItem } from '../static/data'
 import '../App.css'
 
 export default function Sidebar() {
+    const [active, setActive] = useState("Home");
     return (
         <div className='yt-scrollbar w-60 bg-black h-screen top-0 left-0 text-white p-3 overflow-scroll'>
             <div className='mt-14'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur nemo aliquam, blanditiis dicta, maiores accusamus esse mollitia perferendis, dolore odit officiis non Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum molestiae itaque maiores vel vitae doloremque vero obcaecati laborum tempora. Ab tempore nulla natus illum veritatis nobis ducimus ea nisi quae? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi officia aperiam, maxime porro cupiditate ducimus, quas dolore aliquam doloremque, eos laboriosam reiciendis voluptatum perspiciatis tenetur sunt repellat est ipsa eum. illum rerum unde in modi molestias beatae ab! Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe at hic repellendus nemo optio iusto culpa ab odit, impedit recusandae explicabo excepturi labore molestiae quae totam placeat tenetur, accusantium iste.
+                {
+                    SidebarItem.Top.map((item, index) => {
+                        return (
+                            <div className={`h-10 flex justify-start px-3 rounded-xl items-center cursor-pointer hover:bg-gray-900  my-1 ${item.name === active ? "bg-gray-900" : "bg-black"}`}
+                                key={index}
+                                onClick={() => setActive(item.name)}
+                            >
+                                <span className='mr-5'>{item.icon}</span>
+                                <p className='p-2 text-sm font-medium'>{item.name}</p>
+
+                            </div>
+                        )
+                    })
+                }
             </div>
+            <hr className='my-3 text-state-950' />
+            <div>
+                {
+                    SidebarItem.Middle.map((item, index) => {
+                        return (
+                            <div className={`h-10 flex justify-start px-3 rounded-xl items-center cursor-pointer hover:bg-gray-900  my-1 ${item.name === active ? "bg-gray-900" : "bg-black"}`}
+                                key={index}
+                                onClick={() => setActive(item.name)}
+                            >
+                                <span className='mr-5'>{item.icon}</span>
+                                <p className='p-2 text-sm font-medium'>{item.name}</p>
+
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <hr className='my-3 text-slate-950' />
+            <h2 className='px-3 pb-1'>Explore</h2>
+            <div>
+                {
+                    SidebarItem.Explore.map((item, index) => {
+                        return (
+                            <div className={`h-10 flex justify-start px-3 rounded-xl items-center cursor-pointer hover:bg-gray-900  my-1 ${item.name === active ? "bg-gray-900" : "bg-black"}`}
+                                key={index}
+                                onClick={() => setActive(item.name)}
+                            >
+                                <span className='mr-5'>{item.icon}</span>
+                                <p className='p-2 text-sm font-medium'>{item.name}</p>
+
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <hr className='my-3' />
         </div>
     )
 }
