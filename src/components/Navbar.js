823b0ebe1945d7183f1c2ef8ca0bb5e2f5dba8f3
svivek19 <svivek030503@gmail.com>
2024-01-12 11:52:51 +0530

    fix: Mobile responsive navbar signIn button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ const Navbar = () => {
 
   // console.log(user);
 
-  const handleLogout = async () =>{
+  const handleLogout = async () => {
     dispatch(logout())
     await signOut(auth);
   }
@@ -31,7 +31,7 @@ const Navbar = () => {
 
 
   return (
-    <div className='bg-black h-14 flex items-center pl-4 pr-5 justify-between fixed w-full z-10'>
+    <div className='bg-black h-14 flex items-center pl-4 pr-5 md:justify-between fixed w-full z-10'>
       <div className='flex items-center justify-between'>
         <div className='text-white p-2 w-10 text-2xl text-center dim-hover rounded-full cursor-pointer'>
           <HiOutlineBars3 />
@@ -45,14 +45,14 @@ const Navbar = () => {
       </div>
 
       <div className='h-10 flex flex-row items-center'>
-        <div className='w-full md:w-[594px] bg-black lg:flex md:border md:border-slate-900 items-center rounded-3xl h-10'>
-          <input type='text' placeholder='Search' className='w-full hidden md:block bg-black h-6 ml-6 text-white text-start focus:outline-none pl-4' />
+        <div className='w-full md:w-[594px] bg-black hidden md:block lg:flex md:border md:border-slate-900 items-center rounded-3xl h-10'>
+          <input type='text' placeholder='Search' className='w-full bg-black h-6 ml-6 text-white text-start focus:outline-none pl-4' />
           <button className='md:w-16 h-10 md:dim-gray px-1 py-0.5 rounded-r-3xl md:border-l-2 md:border-slate-900'>
             <HiMagnifyingGlass size={22} className='text-white md:mx-auto text-center' />
           </button>
         </div>
       </div>
-      <div className='flex items-center justify-center'>
+      <div className='flex items-center nav-img md:mx-0 justify-center'>
         <div className='flex flex-row items-center'>
           <div className='mr-2 p-2 w-10 dim-hover rounded-full hidden md:block cursor-pointer'>
             <BiVideoPlus className='text-white text-center' size={25} />
@@ -60,7 +60,7 @@ const Navbar = () => {
           <div className='mx-3 p-2 w-10 dim-hover rounded-full hidden md:block cursor-pointer'>
             <FaRegBell size={20} className='text-white text-center hidden md:block' />
           </div>
-          <div className='mr-5 md:mr-0 md:mx-3 items-center cursor-pointer'>
+          <div className='items-center mx-0 cursor-pointer'>
             {
               !user ? (
                 <button className='bg-red-600 py-1 px-4 text-white rounded-md'
@@ -68,20 +68,17 @@ const Navbar = () => {
                   Sign In
                 </button>
               ) : (
-                <img 
-                src={user.photoURL} 
-                alt={user.displayName} 
-                className='objject-contain rounded-full cursor-pointer w-10 h-10' 
-                onClick={handleLogout}
+                <img
+                  src={user.photoURL}
+                  alt={user.displayName}
+                  className='objject-contain rounded-full cursor-pointer w-10 h-10'
+                  onClick={handleLogout}
                 />
               )
             }
-
           </div>
         </div>
       </div>
-
-
     </div>
   )
 }
